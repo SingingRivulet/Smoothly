229a1e091ad0eb3c5dd5e71b27da6dce03c63ce3
cgoxopx <cgoxopx@qq.com>
2020-03-20 12:48:30 +0800

    优化着色器

--- src/shader/cloud.ps.glsl
+++ src/shader/cloud.ps.glsl
@@ -2,72 +2,14 @@ varying vec3 camera;
 varying vec3 pointPosition;//坐标
 varying vec4 screen;
 uniform int  time;
-
-float t = 100.0/60.0;
-
-float function 			= mod(t,4.0);
-bool  multiply_by_F1	= mod(t,8.0)  >= 4.0;
-bool  inverse				= mod(t,16.0) >= 8.0;
-float distance_type	= mod(t/16.0,4.0);
-
-vec2 hash( vec2 p ){
-        p = vec2( dot(p,vec2(127.1,311.7)),dot(p,vec2(269.5,183.3)));
-        return fract(sin(p)*43758.5453);
-}
-
-float voronoi( in vec2 x ){
-    vec2 n = floor( x );
-    vec2 f = fract( x );
-
-    float F1 = 8.0;
-    float F2 = 8.0;
-
-    for( int j=-1; j<=1; j++ )
-        for( int i=-1; i<=1; i++ ){
-            vec2 g = vec2(i,j);
-            vec2 o = hash( n + g );
-
-            o = 0.5 + 0.41*sin( 100.0 + 6.2831*o );
-            vec2 r = g - f + o;
-
-            float d = 	distance_type < 1.0 ? dot(r,r)  :				// euclidean^2
-                                              distance_type < 2.0 ? sqrt(dot(r,r)) :			// euclidean
-                                                                    distance_type < 3.0 ? abs(r.x) + abs(r.y) :		// manhattan
-                                                                                          distance_type < 4.0 ? max(abs(r.x), abs(r.y)) :	// chebyshev
-                                                                                                                0.0;
-
-            if( d<F1 ) {
-                F2 = F1;
-                F1 = d;
-            } else if( d<F2 ) {
-                F2 = d;
-            }
-        }
-
-    float c = function < 1.0 ? F1 :
-                               function < 2.0 ? F2 :
-                                                function < 3.0 ? F2-F1 :
-                                                                 function < 4.0 ? (F1+F2)/2.0 :
-                                                                                  0.0;
-
-    if( multiply_by_F1 )	c *= F1;
-    if( inverse )			c = 1.0 - c;
-
-    return c;
-}
-
-float fbm( in vec2 p ){
-    float s = 0.0;
-    float m = 0.0;
-    float a = 0.5;
-
-    for( int i=0; i<6; i++ ){
-        s += a * voronoi(p);
-        m += a;
-        a *= 0.5;
-        p *= 2.0;
-    }
-    return s/m;
+float raySphereIntersect(vec3 start, vec3 dir , float r){
+    vec3 oc = start;
+    float projoc = dot(dir, oc);
+    float oc2 = dot(oc, oc);
+    float distance2 = oc2 - projoc * projoc;   //计算出的球心到射线的距离
+    float discriminant = r*r - distance2;  //使用勾股定理，计算出另一条边的长度
+    discriminant = sqrt(discriminant);
+    return projoc + discriminant;
 }
 float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
 vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
